70e5a700fc910855776bd51cd248be676ea43bb2
thomasboyle <thomas.paul.boyle@gmail.com>
2026-09-16 08:44:20 +0100

    fix: hide dock in 50 ms
    * fix: hide dock in 50 ms
    
    * fix: hide dock in 50 ms

--- src/DockApp.cpp
+++ src/DockApp.cpp
@@ -21,7 +21,7 @@ DockApp* DockApp::s_instance = nullptr;
 namespace {
 
 constexpr double kShowDurationSeconds = 0.050;
-constexpr double kHideDurationSeconds = 0.200;
+constexpr double kHideDurationSeconds = 0.050;
 constexpr double kDragThresholdLogicalPixels = 6.0;
 constexpr int kBottomHotZonePixels = 2;
 constexpr BYTE kInputWindowAlpha = 1;
